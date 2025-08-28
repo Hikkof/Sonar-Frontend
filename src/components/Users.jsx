86f8db2c4d970781d6f04cf648c2f2a9e18e43f4
Hikkof <108770694+Hikkof@users.noreply.github.com>
2025-08-28 21:22:53 +0200

    Adding "key" prop for element in iterator

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -16,11 +16,12 @@ export function Users() {
             <h2>Users</h2>
             <ul>
                 {users.map((user) => (
-                    <li id={user.id}>
+                    <li key={user.id}>
                         {user.name}
                     </li>
                 ))}
             </ul>
         </div>
     );
+
 }
